fb43b642940d601bbfbf3e57a2b873b6c885ff14
Alex Lombry <alex.lombry@gmail.com>
2017-03-21 23:41:08 +0100

    Functions : Working with it

--- PlayGround/Functions.playground/Contents.swift
+++ PlayGround/Functions.playground/Contents.swift
@@ -42,3 +42,96 @@ func printLetters(word: String) {
 }
 
 printLetters(word: "Salut")
+
+
+// Variadic parameters
+
+func average(someNums: Double...) -> Double {
+    var total: Double = 0
+    
+    for num in someNums {
+        total += num
+    }
+    
+    return total / Double(someNums.count)
+}
+
+
+average(someNums: 273,282,278,378,640,837)
+
+func multiplySomeNumbers(num1: Int = 4, num2: Int = 6) -> Int {
+    return num1 * num2
+}
+
+multiplySomeNumbers()
+
+multiplySomeNumbers(num1: 34)
+
+// returning a tuple
+
+func smallLarge(arrayOfInts: [Int]) -> (min: Int, max: Int)? {
+    
+    if arrayOfInts.isEmpty { return nil }
+    
+    var minimum = arrayOfInts[0]
+    var maximum = arrayOfInts[0]
+    
+    for value in arrayOfInts[1..<arrayOfInts.count] {
+        if value < minimum {
+            minimum = value
+        } else if value > maximum {
+            maximum = value
+        }
+    }
+    
+    return (minimum, maximum)
+}
+
+
+let orderedSize = smallLarge(arrayOfInts: [1,3,6,3,8,19])
+
+var emptyArray = [Int]()
+
+if let orderedSize = smallLarge(arrayOfInts: emptyArray) {
+    print("Minimum : \(orderedSize.min)")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
