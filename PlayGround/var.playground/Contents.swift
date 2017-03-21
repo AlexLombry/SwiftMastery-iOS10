6c2876a55660a47d1059747c0ee59394fbd2ea83
Alex Lombry <alex.lombry@gmail.com>
2017-03-21 18:44:04 +0100

    Add playground folder

---
+++ PlayGround/var.playground/Contents.swift
@@ -0,0 +1,73 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+let maxNumberOf = 5
+
+var attemps = 3
+
+// declare multiple
+
+var a = 2, b = 0, c = 1
+
+let d = 2.2, e = 1.1, f = 3.3
+
+var helloSwift3: String
+
+// Defining related variables of the same type on one line
+var north, south, east, west: String
+
+// Type Inference (detect the type of a variable)
+var some = "This is type inference"
+
+var other: String = "This is not type inference"
+
+var bonjour = "Hello"
+
+bonjour = "Hi"
+
+var name = "Robert"
+
+// String interpolation with \(varName)
+print("Name is \(name)")
+
+var temperature: Int = 80
+var temp = 85
+
+var pi: Float = 3.14
+
+// Double more precise than a float
+var longitude: Double = 5.3456354
+
+// Converting a variable using a cast
+let three = 3
+let fraction = 0.14159
+
+// casting Int three to double and do an explicit conversion
+let pi2 = Double(three) + fraction
+
+// Changing datatype
+let intPi = Int(pi)
+
+// Working with Bool
+
+let sunny = true
+let isRaining = false
+
+if sunny {
+    print("Beach day now")
+} else {
+    print("Umbrella needed")
+}
+
+
+
+
+
+
+
+
+
