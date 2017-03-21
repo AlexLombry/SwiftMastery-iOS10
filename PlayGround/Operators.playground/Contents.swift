8e207710eac0deb1a58d2d4afa0a650c754bb491
Alex Lombry <alex.lombry@gmail.com>
2017-03-21 23:06:15 +0100

    Operators : Increment, Decrement, Minus Unary, Compound Assignment

---
+++ PlayGround/Operators.playground/Contents.swift
@@ -0,0 +1,89 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Binary Operator (between two target example 1 + 2)
+let a = 1 + 2
+
+// Unary / Prefix operator
+-a
+
+// Ternary
+var ternary = true
+
+var string1 = "String 1 gers returned"
+var string2 = "String 2 gets returned"
+
+ternary ? string1 : string2
+
+// Assignment Operator
+var d = 8
+var e = 6
+
+d = e
+
+// Arithmetic
+3 + 3
+6 - 2
+2 * 4
+8 / 2
+
+"Hello" + " World"
+
+// Remainder
+
+9 % 4
+-9 % 4
+
+var i = 0
+
+// Unary Minus
+
+let two = 2
+let minusTwo = -two
+
+two + minusTwo
+
+var c = 1
+
+// Compound assignement
+c += 2
+c -= 1
+c *= 3
+c /= 2
+
+// Comparaison
+
+var f = 3
+c == f
+
+c != f
+
+c > f
+c < f
+c <= f
+c >= f
+
+let name = "John"
+
+if name == "John" {
+    print ("is true")
+} else {
+    print("is false")
+}
+
+
+let gpsDir = "north"
+// Optional, may or may not contain a value
+var paperMapDir: String?
+
+// Nil coalescing operator (paperMapDir first to be evaluated)
+var navigate = paperMapDir ?? gpsDir
+
+
+
+
+
+
