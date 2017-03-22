b0156c93b4150a75a7d3cd445711c33cbb70b57d
Alex Lombry <alex.lombry@gmail.com>
2017-03-22 17:45:11 +0100

    Generics : Part 2

--- PlayGround/Generics.playground/Contents.swift
+++ PlayGround/Generics.playground/Contents.swift
@@ -32,4 +32,95 @@ swap(&arrayFirstNames, &arrayLastNames)
 print(arrayFirstNames)
 print(arrayLastNames)
 
+// * Custom generic types (class struct enum)
+
+// generic stack
+/*
+struct StackOfStrings {
+    var objects = [String]()
+    
+    // mutating because they need to modify the struct array given
+    mutating func push(object: String) {
+        objects.append(object)
+    }
+    
+    mutating func pop() -> String {
+        return objects.removeLast()
+    }
+}
+*/
+
+// Generic struct
+struct StackOfObjects <Element>{
+    var objects = [Element]()
+    
+    // mutating because they need to modify the struct array given
+    mutating func push(object: Element) {
+        objects.append(object)
+    }
+    
+    mutating func pop() -> Element {
+        return objects.removeLast()
+    }
+}
+
+// instance of the struct
+var stringStack = StackOfObjects<String>()
+stringStack.push(object: "France")
+stringStack.push(object: "Allemagne")
+stringStack.push(object: "Belgique")
+
+print(stringStack)
+
+let fromtheTop = stringStack.pop()
+print(stringStack)
+
+
+var intStack = StackOfObjects<Int>()
+intStack.push(object: 2)
+intStack.push(object: 4)
+intStack.push(object: 6)
+intStack.push(object: 8)
+print(intStack)
+
+intStack.pop()
+print(intStack)
+
+var textField = StackOfObjects<UITextField>()
+var sliderStack = StackOfObjects<UISlider>()
+
+// * Extending a Generic
+extension StackOfObjects {
+    var top: Element? {
+    return objects.isEmpty ? nil : objects[objects.count - 1]
+    }
+}
+
+print(intStack.top as Any)
+
+if let topObject = intStack.top {
+    print("The top object on the stack is \(topObject).")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
