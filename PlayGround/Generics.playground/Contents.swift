5475fef0f4fcff68b820c388fff475b05a80ce94
Alex Lombry <alex.lombry@gmail.com>
2017-03-22 17:31:26 +0100

    Generics : Part 1

--- PlayGround/Generics.playground/Contents.swift
+++ PlayGround/Generics.playground/Contents.swift
@@ -2,4 +2,34 @@
 
 import UIKit
 
+func swap<T>(value1: inout T, value2: inout T) {
+    let temp = value1
+    value1 = value2
+    value2 = temp
+}
+
+var int1 = 50
+var int2 = 100
+
+swap(value1: &int1, value2: &int2)
+
+print(int1)
+print(int2)
+
+var someStringValue = "UPPERCASE VALUE"
+var anotherStringValue = "lowercase value"
+
+swap(&someStringValue, &anotherStringValue)
+
+print(someStringValue)
+print(anotherStringValue)
+
+var arrayFirstNames = ["bill", "bob", "mike"]
+var arrayLastNames = ["smith", "jackson", "harris"]
+
+swap(&arrayFirstNames, &arrayLastNames)
+
+print(arrayFirstNames)
+print(arrayLastNames)
+
 
