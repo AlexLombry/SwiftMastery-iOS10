90a2835b84a601ac9ecccb41376b1eed8fa1572c
Alex Lombry <alex.lombry@gmail.com>
2017-03-24 00:03:03 +0100

    Closures : Part 3

--- PlayGround/Closures.playground/Contents.swift
+++ PlayGround/Closures.playground/Contents.swift
@@ -47,4 +47,54 @@ function ($str1, $str2): bool {
 
 // { (str1: String, str2: String) -> Bool in return str1 > str2 }
 
+// Trainling closures
+lastNames.sorted{(str1: String, str2: String) -> Bool in
+    return str1 > str2
+}
+
+let digits = [0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"]
+
+
+let numbers = [25, 82, 615]
+
+let strings = numbers.map { (number) -> String in
+    var number = number
+    var output = ""
+    
+    repeat {
+        output = digits[number % 10]! + output
+        
+        number /= 10
+    } while number > 0
+    
+    return output
+}
+
+//print(strings)
+
+
+
+
+func makeIncrementer(forIncrement amount: Int) -> () -> Int {
+    var runningTotal = 0
+    
+    func incrementer() -> Int {
+        runningTotal += amount
+        
+        return runningTotal
+    }
+    return incrementer
+}
+
+let inc = makeIncrementer(forIncrement: 5)
+
+inc()
+inc()
+inc()
+
+let inc8 = makeIncrementer(forIncrement: 8)
+inc8()
+inc8()
+inc8()
+
 
